aeacc5095a6850067ece44a2469c3c455a92560f
minniear <lukeminniear@gmail.com>
2023-02-23 15:45:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-test-mailparser
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-cqhn5t)
---
+++ src/routes/Counter.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { spring } from 'svelte/motion';
+	import fs from 'file-system'
+	import { MailParser } from 'mailparser'
+
+// const mailpath = process.argv[2];
+
+let parser = new MailParser();
+let input = fs.createReadStream('/home/projects/sveltejs-kit-template-default-cqhn5t/src/nodemailer.eml')
+let mailobj = {
+	attachments: [],
+    text: {}
+};
+	
+input.pipe(parser);
+const parsing = new Promise((resolve) => {
+
+
+parser.on('headers', headers => {
+    let headerObj = {};
+    for (let [k, v] of headers) {
+        // We don’t escape the key '__proto__'
+        // which can cause problems on older engines
+        headerObj[k] = v;
+    }
+
+    mailobj.headers = headerObj;
+});
+
+parser.on('data', data => {
+    if (data.type === 'attachment') {
+        mailobj.attachments.push(data);
+        data.content.on('readable', () => data.content.read());
+        data.content.on('end', () => data.release());
+    } else {
+        mailobj.text = data;
+    }
+});
+
+parser.on('content-type', content => {
+	console.log(content)
+
+})
+
+parser.on('end', () => {
+    console.log(mailobj.headers['content-type'].params);
+		resolve();
+});
+})
+</script>
+
+<div class="counter">
+	{#await parsing}
+	Loading...
+	{:then mailobj}
+	{mailobj.text}
+	{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
+
+</div>
